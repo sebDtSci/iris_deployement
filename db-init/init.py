4705f8f2f912c45860d3f07ff4d6ee9fac5269e8
Seb <121176289+sebDtSci@users.noreply.github.com>
2023-12-14 13:52:22 +0100

    db add

---
+++ db-init/init.py
@@ -0,0 +1,30 @@
+import psycopg2
+from sklearn import datasets
+
+# Charger le dataset Iris
+iris = datasets.load_iris()
+data = iris.data
+target = iris.target
+
+# Établir une connexion à la base de données
+conn = psycopg2.connect(
+    dbname="nom_de_la_bdd", 
+    user="utilisateur", 
+    password="motdepasse", 
+    host="adresse_hote"
+)
+cursor = conn.cursor()
+
+# Insertion des données
+for i in range(len(data)):
+    cursor.execute(
+        "INSERT INTO iris (sepal_length, sepal_width, petal_length, petal_width, species) VALUES (%s, %s, %s, %s, %s)",
+        (data[i][0], data[i][1], data[i][2], data[i][3], iris.target_names[target[i]])
+    )
+
+# Valider la transaction
+conn.commit()
+
+# Fermer la connexion
+cursor.close()
+conn.close()
---
+++ db-init/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE iris (
+    id SERIAL PRIMARY KEY,
+    sepal_length FLOAT,
+    sepal_width FLOAT,
+    petal_length FLOAT,
+    petal_width FLOAT,
+    species VARCHAR(50)
+);
